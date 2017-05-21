1e1c0d85564c33745a543c830afdd03c88c6ef5f
alf12151215 <nanonature@Nano-Nature.local>
2017-05-21 09:56:00 +0900

    #add prep

--- wp-content/themes/yoko_underscore/footer.php
+++ wp-content/themes/yoko_underscore/footer.php
@@ -15,9 +15,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'yoko_underscore' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'yoko_underscore' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'yoko_underscore' ), 'yoko_underscore', '<a href="https://automattic.com/" rel="designer">Underscores.me</a>' ); ?>
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'yoko_underscore' ) ); ?>">(c)yokosato</a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
